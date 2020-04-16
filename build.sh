91223d56dea150e9747ecbba2d809224a8e8df44
mayur-sn <50200277+mayur-sn@users.noreply.github.com>
2020-04-16 10:25:16 +0530

    Update build.sh
    mysql--->mysql1

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo docker build ./mysql/ -t db
-sudo docker run -it -d -p 3306:3306 --name mysql db
+sudo docker build ./mysql1/ -t db
+sudo docker run -it -d -p 3306:3306 --name mysql1 db
 
 sudo docker build ./website/ -t web
-sudo docker run -it -d -p 3001:80 --link mysql:db web
+sudo docker run -it -d -p 3001:80 --link mysql1:db web
